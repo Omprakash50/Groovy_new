5f4c6a29310341e398a64a90cec7bbd7bec74552
Omprakash choudhari <56760981+Omprakash50@users.noreply.github.com>
2020-07-03 11:56:54 +0530

    Create d1.groovy

---
+++ d1.groovy
@@ -0,0 +1,77 @@
+job("task6_ob1") {
+  description("Download the data from github")
+               scm {
+                       github('Omprakash50/kubernetes')
+                    }
+               triggers {
+                       scm(" * * * * * ")
+                    }
+               steps {
+                       shell("sudo cp * /kuberada/")
+                    }
+}
+job("task6_ob2") {
+     description("checking the code and launching the pods")
+     triggers {
+                 upstream('task6_job1' , 'SUCCESS')
+        }
+      steps {
+             shell('''if sudo ls /kuberada/ | grep .php
+then
+if sudo kubectl get pods | grep php-container
+then
+echo "already running"
+output=$(sudo kubectl get pods | grep php-container | cut -d ' ' -f 1)
+sudo kubectl cp /kuberada/first.html $output:/var/www/html/
+else
+sudo kubectl apply -f /root/php.yml
+sleep 30
+output=$(sudo kubectl get pods | grep php-container | cut -d ' ' -f 1)
+sudo kubectl cp /kuberada/first.html $output:/var/www/html/
+fi
+elif sudo ls /kuberada/ | grep .html
+then
+if sudo kubectl get pods | grep html-container
+then
+echo "already running"
+output=$(sudo kubectl get pods | grep html-container | cut -d ' ' -f 1)
+sudo kubectl cp /kuberada/first.html $output:/var/www/html/
+else
+sudo kubectl apply -f /root/html.yml
+sleep 30
+output=$(sudo kubectl get pods | grep html-container | cut -d ' ' -f 1)
+sudo kubectl cp /kuberada/first.html $output:/var/www/html/
+fi
+else
+sudo echo "Don't have environment for this file"
+fi''')
+         }
+}
+job("task6_ob3") {
+      description("Testing the code by seeing its status code")
+      triggers {
+                upstream('task6_job2' , 'SUCCESS')
+         }
+      steps {
+             shell('''if sudo ls /kuberada/ | grep .php
+then
+status=$(curl -s -i -w "%{http_code}" -o /dev/null 192.168.99.107:30000/first1.php)
+elif sudo ls /kuberada/ | grep .html
+then
+status=$(curl -s -i -w "%{http_code}" -o /dev/null 192.168.99.107:30000/first1.html)
+else
+sudo echo "Don't have environment for this file"
+fi
+
+if [[ $status == 200 ]] ; then exit 0  ; else sudo curl --user 'admin:Omkar@jio.com1' http://192.168.43.50:8080/job/Notify/build?token=mail ;fi''')
+      }    
+}
+job("task6_ob4") {
+      description("Sending Email if error occur in the code otherwise not")
+      triggers {
+                  upstream('task6_job3' , 'SUCCESS')
+            }
+      steps {
+                  shell('''sudo python3 /root/mail.py''')
+            }
+}            
